8bc1348cbd4a5c1ca0cf469afb31c0a8a03fb2a3
Khristoffer Julio <khristoffer.julio@rsvp.com.au>
2018-11-14 13:46:22 +0800

    rsvp: update RSVProfileSummaryCell to show member details on chat page

--- Sources/Layout/RSVPProfileSummarySizeCalculator.swift
+++ Sources/Layout/RSVPProfileSummarySizeCalculator.swift
@@ -9,13 +9,45 @@
 import Foundation
 
 open class RSVPProfileSummarySizeCalculator: TextMessageSizeCalculator {
-    open override func messageContainerMaxWidth(for message: MessageType) -> CGFloat {
-        // TODO: check the RSVPSystemMessageSizeCalculator implementation
-        return 10
-    }
-    
     open override func messageContainerSize(for message: MessageType) -> CGSize {
-        // TODO: check the RSVPSystemMessageSizeCalculator implementation
-        return CGSize(width: 10, height: 10)
+        let maxWidth = messageContainerMaxWidth(for: message)
+        let messageInsets = messageLabelInsets(for: message)
+        let attributedTitleText: NSAttributedString
+        let attributedSummaryText: NSAttributedString
+        
+        // we only limit 2 line description
+        let maxLineHeight: CGFloat = {
+            let attribute: [NSAttributedString.Key : Any]? = [.font: UIFont.systemFont(ofSize: 13)]
+            let msgSize = labelSize(for: NSAttributedString(string: "one liner text", attributes: attribute), considering: maxWidth)
+            return msgSize.height * 2
+        }()
+        
+        var messageContainerSize: CGSize
+        var bioTextHeight: CGFloat = 0
+        
+        switch message.kind {
+        case .rsvpProfileSummary(let summary):
+            attributedTitleText = NSAttributedString(string: summary.title, attributes: [.font: UIFont.systemFont(ofSize: 15)])
+            if let summary = summary.text, summary.replacingOccurrences(of: " ", with: "") != "" {
+                attributedSummaryText = NSAttributedString(string: summary, attributes: [.font: UIFont.systemFont(ofSize: 13)])
+                bioTextHeight = labelSize(for: attributedSummaryText, considering: maxWidth).height
+            }
+        default:
+            fatalError("messageContainerSize received unhandled MessageDataType: \(message.kind)")
+        }
+        
+        messageContainerSize = labelSize(for: attributedTitleText, considering: maxWidth)
+        messageContainerSize.width = super.messageContainerMaxWidth(for: message)
+        messageContainerSize.height += messageInsets.vertical
+        
+        // add padding to title label
+        messageContainerSize.height += (RSVP_SystemMsgCellPadding + RSVP_SystemMsgCellPadding)
+        
+        let maxHeight: CGFloat = maxLineHeight
+        if bioTextHeight > 0 {
+            messageContainerSize.height += bioTextHeight > maxHeight ? maxHeight : bioTextHeight
+        }
+        
+        return messageContainerSize
     }
 }
--- Sources/Views/Cells/RSVPProfileSummaryCell.swift
+++ Sources/Views/Cells/RSVPProfileSummaryCell.swift
@@ -9,5 +9,53 @@
 import UIKit
 
 open class RSVPProfileSummaryCell: MessageContentCell {
-
+    open var titleLabel = MessageLabel()
+    open var aboutLabel = UILabel()
+    
+    open override func setupSubviews() {
+        super.setupSubviews() 
+        messageContainerView.addSubview(titleLabel)
+        messageContainerView.addSubview(aboutLabel)
+        setupConstraints()
+    }
+    
+    open func setupConstraints() {
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        aboutLabel.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: messageContainerView.topAnchor, constant: RSVP_SystemMsgCellPadding),
+            titleLabel.leadingAnchor.constraint(equalTo: messageContainerView.leadingAnchor, constant: RSVP_SystemMsgCellPadding),
+            titleLabel.trailingAnchor.constraint(equalTo: messageContainerView.trailingAnchor, constant: -RSVP_SystemMsgCellPadding),
+            aboutLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 0),
+            aboutLabel.leadingAnchor.constraint(equalTo: messageContainerView.leadingAnchor, constant: RSVP_SystemMsgCellPadding),
+            aboutLabel.trailingAnchor.constraint(equalTo: messageContainerView.trailingAnchor, constant: -RSVP_SystemMsgCellPadding),
+            aboutLabel.bottomAnchor.constraint(equalTo: messageContainerView.bottomAnchor, constant: -RSVP_SystemMsgCellPadding)
+        ])
+    }
+    
+    open override func configure(with message: MessageType, at indexPath: IndexPath, and messagesCollectionView: MessagesCollectionView) {
+        super.configure(with: message, at: indexPath, and: messagesCollectionView)
+        guard let displayDelegate = messagesCollectionView.messagesDisplayDelegate else {
+            fatalError(MessageKitError.nilMessagesDisplayDelegate)
+        }
+        
+        let textColor = displayDelegate.textColor(for: message, at: indexPath, in: messagesCollectionView)
+        titleLabel.textColor = textColor
+        titleLabel.font = UIFont.systemFont(ofSize: 15, weight: UIFont.Weight.bold)
+        titleLabel.textAlignment = .center
+        
+        aboutLabel.textColor = textColor
+        aboutLabel.font = UIFont.systemFont(ofSize: 13)
+        aboutLabel.textAlignment = .center
+        aboutLabel.lineBreakMode = .byTruncatingTail
+        aboutLabel.numberOfLines = 2
+        
+        if case .rsvpProfileSummary(let summary) = message.kind {
+            titleLabel.text = summary.title
+            guard summary.text?.replacingOccurrences(of: " ", with: "") != "" else {
+                return
+            }
+            aboutLabel.text = summary.text
+        }
+    }
 }
